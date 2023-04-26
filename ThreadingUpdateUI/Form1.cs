5867237620acae4965fd224f017056fac39ccfca
stanley edore <edorestanley@gmail.com>
2023-04-26 12:13:42 -0300

    working on threading

--- ThreadingUpdateUI/Form1.cs
+++ ThreadingUpdateUI/Form1.cs
@@ -18,15 +18,58 @@ namespace ThreadingUpdateUI
             InitializeComponent();
         }
 
-      
+      private List<Label> threadLabels = new List<Label>();
         private void btnGo_Click(object sender, EventArgs e)
         {
+            if(threadLabels.Count < 100)
+            {
+                int.TryParse(textBox1.Text, out int iterationCount);
+                int.TryParse(textBox2.Text, out int sleepDuration);
 
+                Label l = new Label();
+                l.Text = DateTime.Now.ToLongTimeString();
+                threadLabels.Add(l);
+
+                l.Top = (panel1.Controls.Count) * l.Height + 20;
+                l.Left = 5;
+
+                this.panel1.Controls.Add(l);
+
+                Task.Factory.StartNew(() =>
+                {
+                    for (int i = 0; i < iterationCount; i++)
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            l.Text = i.ToString();
+                        });
+
+                        Thread.Sleep(sleepDuration);
+
+                    }
+                });
+            }
         }
 
         private void btnGoNoThread_Click(object sender, EventArgs e)
         {
+            int.TryParse(textBox1.Text, out int iterationCount);
+            int.TryParse(textBox2.Text, out int sleepDuration);
 
+            btnGoNoThread.Enabled= false;
+            textBox1.Enabled= false;
+            textBox2.Enabled= false;
+
+            for (int i = 0; i <= iterationCount; i++)
+            {
+                lblCounter.Text = i.ToString();
+                lblCounter.Refresh();
+                Thread.Sleep(sleepDuration);
+            }
+
+            btnGoNoThread.Enabled = Enabled;
+            textBox1.Enabled = Enabled;
+            textBox2.Enabled = Enabled;
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
